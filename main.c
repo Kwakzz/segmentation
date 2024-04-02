3f73fc98d284f35d1d715c2e0a1b3575c810f461
Kwakzz <kwakuosafo20@gmail.com>
2024-04-02 20:19:10 +0000

    defined PCB and redefined segment

--- main.c
+++ main.c
@@ -9,12 +9,20 @@
 
 // NB: assume all ints pertaining to sizes are in MB.
 
+typedef enum {
+    CODE,
+    STACK,
+    HEAP
+} SegmentType;
+
 // STRUCT DEFINTIONS
 typedef struct {
-    int size;
-    int base; // starting physical address 
+    // address range must be within its process' address range.
+    int limit; // determines the ending address. base + limit = ending address
+    int base; // starting physical address.
     int growth_direction; // 1 indicates an upward direction, -1 indicates a downward direction, 0 for no growth
     bool protection_bit; // 1 indicates read-write, 0 indicates read-only
+    SegmentType type;
 } Segment;
 
 
@@ -25,11 +33,23 @@ typedef struct {
 } Process;
 
 
+typedef struct {
+    int id;
+    int size; // determines the ending address. start address + size = ending address
+    int base; // starting address
+    int status; // 0 for ready, 1 for running, 2 for blocked
+} ProcessControlBlock; // contains metadata about process
+
+ProcessControlBlock* process_table[20]; // contains process control blocks. 
+
+
 
 // FUNCTION DECLARATIONS
 int generate_random_number(int min, int max);
-Segment* create_segment(int max_segment_size, int growth_direction, int protection_bit);
+Segment* create_segment(int max_segment_size, SegmentType type);
 Process* create_process (int id);
+ProcessControlBlock create_PCB (Process process);
+void set_segment_type(SegmentType type, Segment *segment);
 
 
 
@@ -38,13 +58,15 @@ int main () {
     srand(time(NULL)); // for random number generation. must run just once.
 
     Process* process = create_process(1);
+
     printf("Process size: %d\n", process->size);
+
     for (int i = 0; i<3; i++) {
         printf("SEGMENT %d\n", i);
-        printf("Segment growth direction: %d\n", process->segments[i]->growth_direction);
-        printf("Segment protection direction: %d\n", process->segments[i]->protection_bit);
-        printf("Segment size: %d\n", process->segments[i]->size);
+        printf("Segment type: %d\n", process->segments[i]->type);
+        printf("Segment size: %d\n", process->segments[i]->limit);
     }
+
     free(process);
 
     return 0;
@@ -59,7 +81,7 @@ Process* create_process (int id) {
 
     if (process == NULL) {
         printf("Error creating process.\n");
-        return NULL;
+        exit(1);
     }
 
     process->size = process_size;
@@ -67,32 +89,72 @@ Process* create_process (int id) {
 
     int max_segment_size = process_size/NO_OF_SEGMENTS;
 
-    Segment* code = create_segment(max_segment_size, 0, 0);
-    Segment* stack = create_segment(max_segment_size, 1, 1);
-    Segment* heap = create_segment(max_segment_size, -1, 1);
+    process->segments[0] = create_segment(max_segment_size, CODE);
+    process->segments[1] = create_segment(max_segment_size, STACK);
+    process->segments[2] = create_segment(max_segment_size, HEAP);
 
-    process->segments[0] = code;
-    process->segments[1] = stack;
-    process->segments[2] = heap;
+    create_PCB(*process);
 
     return process;
 
 }
 
-Segment* create_segment(int max_segment_size, int growth_direction, int protection_bit) {
-    int segment_size = generate_random_number(MIN_SEGMENT_SIZE, max_segment_size);
+ProcessControlBlock create_PCB (Process process) {
+    
+    ProcessControlBlock* PCB = malloc(sizeof(ProcessControlBlock));
+
+    if (PCB == NULL) {
+        printf("Error creating PCB\n");
+        exit(1);
+    }
+
+    PCB->size = process.size;
+    PCB->id = process.id;
+
+    process_table[PCB->id] = PCB;
+
+    return *PCB;
+
+}
+
+Segment* create_segment(int max_segment_size, SegmentType type) {
+
+    int segment_limit = generate_random_number(MIN_SEGMENT_SIZE, max_segment_size);
 
     Segment* segment = malloc(sizeof(Segment));
     if (segment == NULL) {
         printf("Error creating segment.\n");
-        return NULL;
+        exit(1);
     }
 
-    segment->size = segment_size;
-    segment->growth_direction = growth_direction;
-    segment->protection_bit = protection_bit;
+    segment->limit = segment_limit;
+
+    set_segment_type(type, segment);
 
     return segment;
+
+}
+
+void set_segment_type(SegmentType type, Segment *segment) {
+
+    if (type == CODE) {
+        segment->type = CODE;
+        segment->growth_direction = 0;
+        segment->protection_bit = 0;
+    }
+
+    if (type == STACK) {
+        segment->type = STACK;
+        segment->growth_direction = 1;
+        segment->protection_bit = 1;
+    }
+
+    if (type == HEAP) {
+        segment->type = HEAP;
+        segment->growth_direction = -1;
+        segment->protection_bit = 1;
+    }
+
 }
 
 
